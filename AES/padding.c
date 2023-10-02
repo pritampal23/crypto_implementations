b938037a3e377f14f63d754460d9b725499c01bd
pritampal23 <iampritampal23@gmail.com>
2023-10-02 23:03:38 +0530

    Adds padding to the block

---
+++ AES/padding.c
@@ -0,0 +1,14 @@
+#include "aes.h"
+
+void padding(unsigned char *plaintext,uint64_t padding_len, unsigned char *block_message){
+    uint8_t j;
+    uint64_t last_block_size = 16 - padding_len;
+    for(j = 0; j < 16; j++){
+        if(j < last_block_size)
+            block_message[j] = plaintext[j];
+        else if(j == last_block_size)
+            block_message[j] = 0x80;
+        else
+            block_message[j] = 0x00;
+    }
+}
